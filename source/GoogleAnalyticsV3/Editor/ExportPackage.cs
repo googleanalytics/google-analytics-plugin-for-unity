87635e595b42b9ffc616b5aa6d1a1e5bf6472c23
Yuri Visser <yuri@scopely.com>
2015-03-26 11:02:55 -0700

    log edit

--- source/GoogleAnalyticsV3/Editor/ExportPackage.cs
+++ source/GoogleAnalyticsV3/Editor/ExportPackage.cs
@@ -6,56 +6,33 @@ public class ExportPackage
 	[MenuItem("Assets/Export Google Analytics Package", false, 301)]
 	public static void Export()
 	{
-		Debug.Log("Exporting ScopelyPlatform package...");
+		Debug.Log("Google Analytics Package...");
 		AssetDatabase.ExportPackage(new string[] {
-			//scopely platform scripts
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatform.cs",
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatformConfig.cs",
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatformConstants.cs",
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatformEnums.cs",
-			"Assets/ScopelyPlatform/Scripts/IScopelyPlatformNative.cs",
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatformNativeAndroid.cs",
-			"Assets/ScopelyPlatform/Scripts/ScopelyPlatformNativeIos.cs",
-			"Assets/ScopelyPlatform/Scripts/ABTestingServiceNativeAndroid.cs",
-			"Assets/ScopelyPlatform/Scripts/ABTestingServiceNativeIos.cs",
-			"Assets/ScopelyPlatform/Scripts/ABTestingServiceNativeStub.cs",
-			"Assets/ScopelyPlatform/Scripts/IABTestingService.cs",
-			
-			//scopely platform editor
-			"Assets/ScopelyPlatform/Editor/mod_pbxproj.py",
-			"Assets/ScopelyPlatform/Editor/mod_pbxproj.pyc",
-			"Assets/ScopelyPlatform/Editor/ScopelyPlatformConfigWindow.cs",
-			"Assets/ScopelyPlatform/Editor/ScopelyPlatformEditor.cs",
-			"Assets/ScopelyPlatform/Editor/ScopelyPlatformPostProcess.cs",
-			"Assets/ScopelyPlatform/Editor/ScopelyPlatformPostProcess_Ios",
-			
-			"Assets/Editor Default Resources",
-			
-			//titan
-			"Assets/ScopelyPlatform/Scripts/Analytics/Exceptions",
-			"Assets/ScopelyPlatform/Scripts/Analytics/Models",
-			"Assets/ScopelyPlatform/Scripts/Analytics/Titan.cs",
-			"Assets/ScopelyPlatform/Scripts/Analytics/TitanAndroidNative.cs",
-			"Assets/ScopelyPlatform/Scripts/Analytics/TitanIosNative.cs",
-			
-			//urban airship
-			"Assets/ScopelyPlatform/Scripts/UrbanAirship",
-			"Assets/ScopelyPlatform/Editor/XUPorter",
-			
-			//mat & adjust
-			"Assets/ScopelyPlatform/Scripts/Attribution",
-			
-			//native info
-			"Assets/ScopelyPlatform/Scripts/NativeInfo",
-			
-			//Helpshift
-			"Assets/Helpshift",
-			
-			//common
+			//Plugins
 			"Assets/Plugins",
-			"Assets/ScopelyPlatform/Prefabs",
+
+			//GoogleAnalyticsV3 - root
+			"Assets/GoogleAnalyticsV3/Field.cs",
+			"Assets/GoogleAnalyticsV3/Fields.cs",
+			"Assets/GoogleAnalyticsV3/GAIHandler.cs",
+			"Assets/GoogleAnalyticsV3/GAv3.prefab",
+			"Assets/GoogleAnalyticsV3/GoogleAnalyticsAndroidV3.cs",
+			"Assets/GoogleAnalyticsV3/GoogleAnalyticsiOSV3.cs",
+			"Assets/GoogleAnalyticsV3/GoogleAnalyticsMPV3.cs",
+			"Assets/GoogleAnalyticsV3/GoogleAnalyticsV3.cs",
+
+			//GoogleAnalyticsV3 - Attributes
+			"Assets/GoogleAnalyticsV3/Attributes",
+
+			//GoogleAnalyticsV3 - HitBuilders
+			"Assets/GoogleAnalyticsV3/HitBuilders",
+
+			//GoogleAnalyticsV3 - Editor
+			"Assets/GoogleAnalyticsV3/Editor/PostprocessBuildPlayer",
+			"Assets/GoogleAnalyticsV3/Editor/RangedTooltipDrawer.js",
+			"Assets/GoogleAnalyticsV3/Editor/TooltipDrawer.cs"
 		}, 
-		"ScopelyPlatform-Unity.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
-		Debug.Log ("ScopelyPlatform.unitypackage Exported");
+		"google-analytics-unit.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
+		Debug.Log ("Google Analytics Package Exported");
 	}
 }
