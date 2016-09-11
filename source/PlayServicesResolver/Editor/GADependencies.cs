bc6e0ad24e65e609d0e59ddfecc80aab474060e6
Andrew <thebaldwin@google.com>
2016-09-11 12:54:16 -0700

    Add Google Play Services resolver for Unity

---
+++ source/PlayServicesResolver/Editor/GADependencies.cs
@@ -0,0 +1,26 @@
+﻿using Google.JarResolver;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class GADependencies
+{
+    private static readonly string PluginName = "gaUnityPlugin";
+    public static PlayServicesSupport svcSupport;
+
+    static GADependencies()
+    {
+
+        svcSupport = PlayServicesSupport.CreateInstance(
+                                             PluginName,
+                                             EditorPrefs.GetString("AndroidSdkRoot"),
+                                             "ProjectSettings");
+        RegisterDependencies();
+    }
+
+    public static void RegisterDependencies()
+    {
+        //svcSupport.DependOn("com.google.android.gms", "play-services-base", "8.1+");
+        svcSupport.DependOn("com.google.android.gms", "play-services-analytics", "LATEST");
+        //svcSupport.DependOn("com.google.android.gms.analytics", "analytics", "9.2+");
+    }
+}
